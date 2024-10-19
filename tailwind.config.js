bfe740467cf4d1115b94d76853cdc4736b68136b
Layhak <layhakheng@gmail.com>
2024-10-19 20:51:38 +0700

    adding alpine,postcss and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './app/templates/**/*.html', // Include all HTML files in templates directory
+        './app/**/*.py',             // Include Python files if they contain Tailwind classes
+        './src/**/*.js',             // Include any JavaScript files
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
